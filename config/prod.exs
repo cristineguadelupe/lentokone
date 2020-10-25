d9c2d039faa0df92307e2ecc930ee2eebdeb22cb
Cristine Guadelupe <cristineguadelupe@me.com>
2020-10-25 17:20:31 -0300

    Gigalixir config

--- config/prod.exs
+++ config/prod.exs
@@ -25,7 +25,7 @@ config :lentokone, Lentokone.Repo,
 # which you should run after static files are built and
 # before starting your production server.
 config :lentokone, LentokoneWeb.Endpoint,
-  url: [host: "lentokone.gigalixir.com", port: 80],
+  url: [host: "lentokone.gigalixirapp.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
